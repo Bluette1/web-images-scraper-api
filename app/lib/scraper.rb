f672a55a5e79eedfcdb8021d4538f6bddfc4bef5
Bluette1 <marylene.sawyer@gmail.com>
2023-12-19 21:35:52 +0200

    Add Remote Debugging Port
    Fix Error
    ` (session not created: DevToolsActivePort file doesn't exist)
    `

--- app/lib/scraper.rb
+++ app/lib/scraper.rb
@@ -6,6 +6,8 @@ class Scraper
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
+    options.add_argument('--remote-debugging-port=9222')
+
     @driver = Selenium::WebDriver.for(:chrome, options:)
     @driver.get 'https://picjumbo.com/free-stock-photos/nature/page/1'
     @pic_container = @driver.find_elements(tag_name: 'picture')
